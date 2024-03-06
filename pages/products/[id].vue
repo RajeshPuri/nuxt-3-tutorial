e26e6ed4ac36604589526810277b36dc1f09c085
RajeshPuri <razeshpuri@gmail.com>
2024-03-06 14:12:58 +0545

    fetch data from local database

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -12,7 +12,8 @@
 
 <script setup>
   const { id } = useRoute().params
-  const uri = 'https://fakestoreapi.com/products/' + id
+  // const uri = 'https://fakestoreapi.com/products/' + id
+  const uri = '/api/product/' + id
 
   //  fetch the products
   const { data: product } = await useFetch(uri, { key: id })
