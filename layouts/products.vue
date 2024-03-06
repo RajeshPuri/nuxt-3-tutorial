155c2cf10e9345b8e6e23b651cb096d9a8ec229c
RajeshPuri <razeshpuri@gmail.com>
2024-03-06 14:18:52 +0545

    q-fix(menu item added in nav bar)

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <header class="shadow-sm bg-white">
-      <nav class="container mx-auto p-4">
+      <nav class="container mx-auto p-4 flex justify-between">
         <NuxtLink to="/products" class="font-bold">Nuxt Dojo Merch</NuxtLink>
+        <ul class="flex gap-4">
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+          <li><NuxtLink to="/user">User</NuxtLink></li>
+          
+        </ul>
       </nav>
     </header>
     <div class="container mx-auto p-4">
